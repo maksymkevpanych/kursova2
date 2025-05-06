5457d694a5b42426cc51eb1b5bc90815855c5149
maksymkevpanych <kevpanych.maksym@student.uzhnu.edu.ua>
2025-05-06 12:21:30 +0300

    images warehouse_screen, warehouse_stock_screen

--- lib/Screens/goods_screen.dart
+++ lib/Screens/goods_screen.dart
@@ -183,6 +183,12 @@ class _GoodsScreenState extends State<GoodsScreen> {
                               'Опис: ${item['description'] ?? 'Не вказано'}',
                               style: Theme.of(context).textTheme.bodyMedium,
                             ),
+                            Image.network(
+                              item['img_url'] ?? 'https://via.placeholder.com/150',
+                              height: 100,
+                              width: 100,
+                              fit: BoxFit.cover,
+                            ),
                             const SizedBox(height: 16),
                             Align(
                               alignment: Alignment.centerRight,
